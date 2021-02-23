83287d93508c124cb6ca091c9c1983ddac6a2ec7
Ludo loops <ludoloops@protonmail.com>
2021-02-23 17:00:51 +0100

    Feat: first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const session = require('express-session');
+
+const app = express();
+
+const { PORT = 3000 } = process.env;
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
